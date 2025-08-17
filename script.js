09cde860528295e5c33f1714e6935e5c6b24d994
Victor Martins <carlosdzickiss@gmail.com>
2025-08-17 19:50:28 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//fade-up, fade-down, fade-left, fade-right
+
+//zoom-in, zoom-out
+
+//flip-up, flip-left, flip-right 
+
+// EX TAG PARA TROCA EFEITO SÓ COLOCAR NO LUGAR DAS OUTRAS DENTROS DO COIDIGOk
+//data-aos="flip-right"
