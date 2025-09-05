7dd66363ab3dcb40844c0ad8e546a8d9975cf254
Victor Martins <carlosdzickiss@gmail.com>
2025-09-05 14:35:06 -0300

    atuzlizando...

--- script.js
+++ script.js
@@ -6,3 +6,20 @@
 
 // EX TAG PARA TROCA EFEITO SÓ COLOCAR NO LUGAR DAS OUTRAS DENTROS DO COIDIGOk
 //data-aos="flip-right"
+
+const accordionList = document.querySelectorAll('.js-accordion-list dt')
+
+// Fazendo que apareça primeiro elemento da lista (curiosidade)
+accordionList[0].classList.add('ativo')
+accordionList[0].nextElementSibling.classList.add('ativo')
+
+
+accordionList.forEach(itemDT=>{
+    itemDT.addEventListener('click', accordion)
+})
+
+function accordion(){
+    this.classList.toggle('ativo')
+    this.nextElementSibling.classList.toggle('ativo')
+    // O método nextElementdsibling retorna o próximo elemento da lista
+}
